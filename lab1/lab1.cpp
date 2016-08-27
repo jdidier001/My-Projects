faacc55f70681f4c42dc2cb0aa951fa933d44d41
Jared Didier <jdidier001@student.butte.edu>
2016-08-27 03:16:32 +0000

    Lab 1 folder

---
+++ lab1/lab1.cpp
@@ -0,0 +1,55 @@
+/* 
+_________________________________________________
+Jared Didier
+email:jdidier001@student.butte.edu
+
+-------------------------------------------------
+
+_________________________________________________
+1.1 Lab - ASCII Art 
+8-26-16
+
+------------------------------------------------
+
+________________________________________________
+Fall 2016 CSCI 20 – Programming and Algorithms I
+instructor: A Browne
+email: browneap@butte.edu
+--------------------------------------------------
+
+________________________________________________
+Version # 1.0 - 8-26-16
+_________________________________________________
+**Notes**
+
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    cout << "       #         " << endl;
+    cout << "      #*#        " << endl;
+    cout << "     #***#       " << endl;         
+    cout << "    #*****#      " << endl;
+    cout << "     #***#       " << endl;         
+    cout << "     #***#       " << endl;
+    cout << "     #***#       " << endl;
+    cout << "     #***#       " << endl;
+    cout << "     #***#       " << endl;
+    cout << "     #***#       " << endl;
+    cout << "    #*****#      " << endl;
+    cout << "   *#*****#*     " << endl;
+    cout << "  **#*****#**    " << endl;
+    cout << " ***#*****#***   " << endl;
+    cout << "       $         " << endl;
+    cout << "      $$$        " << endl;
+    cout << "     $$$$$       " << endl;         
+    cout << "    $$$$$$$      " << endl;
+
+   return 0;
+}
