bb02f133f7bf34ef326f8eac245f063c7f27b871
Jared Didier <jdidier001@student.butte.edu>
2016-08-30 20:34:05 +0000

    Lab2

---
+++ lab2/lab2.cpp
@@ -0,0 +1,60 @@
+/* 
+_________________________________________________
+Jared Didier
+email:jdidier001@student.butte.edu
+
+-------------------------------------------------
+
+_________________________________________________
+1.1 Lab - ASCII Art 
+8-26-16
+This is a program that will have the user guess 
+a randomly generated number from a set of numbers.
+------------------------------------------------
+
+________________________________________________
+Fall 2016 CSCI 20 – Programming and Algorithms I
+instructor: A Browne
+email: browneap@butte.edu
+--------------------------------------------------
+
+________________________________________________
+Version # 1.0 - 8-26-16
+_________________________________________________
+**Notes**
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    
+    
+numGuessCount = 10;
+
+
+computer will display instructios to user
+
+choose a number between 20 and 70
+
+computer generates number 
+
+player guesses
+
+computer either says correct if true  and awards numGuessCount points 
+then end and resets
+
+else 
+states higher or lower depending on the chosen 
+number - compared to the guess. 
+
+Subtracts a numGuessCount point
+numGuessCount= numGuessCount-1;
+
+loop until true
+
+end and reset
+
