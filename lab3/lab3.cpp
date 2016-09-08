173d1f1dedeb6d379cdec65476f5c333b67df01a
Jared Didier <jdidier001@student.butte.edu>
2016-09-08 03:52:19 +0000

    Lab 4 Submission

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -36,6 +36,9 @@ using namespace std;
 int main() {
 // setting up the variables.
 
+    char userFirstIntialLastName;
+    int userAge;
+    float userDecimal;
     string nounOne = "";
     string nounTwo = "";
     string nounThree = "";
@@ -45,26 +48,39 @@ int main() {
     
 // this is where i ask for the words from the user.
 
+
+cout << "Please input the first intial of your last name  " ;
+cin >> userFirstIntialLastName;
+
+cout << "Please input your age " ;
+cin >> userAge;
+
+cout << "Please enter a decimal ";
+cin >> userDecimal;
+
 cout << "Please input an Noun " ;
 cin >> nounOne;
-cout << "Please input an Plural Noun" ;
+cout << "Please input an Plural Noun " ;
 cin >> nounPlural ;
-cout << "Please input an Noun" ;
+cout << "Please input an Noun " ;
 cin >> nounTwo ;
-cout << "Please input an Place" ;
+cout << "Please input an Place " ;
 cin >> place ;
-cout << "Please input an adjective" ;
+cout << "Please input an adjective " ;
 cin >> adjective;
-cout << "Please input an Noun" ;
+cout << "Please input an Noun " ;
 cin >> nounThree; 
 
 
 // putting the user input together with the story.
-
-cout << "Be kind to your " << nounOne  << " -footed " << nounPlural;
-cout << "For a duck may be somebody`s " << nounTwo;
-cout << "Be kind to your " << nounPlural << "in " << place << ".";
+cout << endl << endl;
+cout << "Hello " << userFirstIntialLastName << endl;
+cout << "Your age is " << userAge << endl;
+cout << "The decimal you chose was " << userDecimal << endl;
+cout << "Be kind to your " << nounOne  << "-footed " << nounPlural<< endl;
+cout << "For a duck may be somebody`s " << nounTwo << endl;
+cout << "Be kind to your " << nounPlural << " in " << place << "." << endl;
 cout << "Where the weather is always " << adjective << endl << endl;
-cout << "You may think that this is the " << nounThree << endl;
+cout << "You may think that this is the " << nounThree << " " << endl;
 cout << "Well it is.";
 }
---
+++ lab4/lab4.cpp
@@ -0,0 +1,98 @@
+/* 
+_________________________________________________
+Jared Didier
+email:jdidier001@student.butte.edu
+
+-------------------------------------------------
+
+_________________________________________________
+1.4 Lab CoinStar Program
+9-2-16
+
+------------------------------------------------
+
+________________________________________________
+Fall 2016 CSCI 20 – Programming and Algorithms I
+instructor: A Browne
+email: browneap@butte.edu
+--------------------------------------------------
+
+________________________________________________
+Version # 1.0 - 9-2-16
+_________________________________________________
+**Notes**
+
+*/
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main() {
+
+
+// the  count of the money zeroed out
+int  quarterCount = 0;
+int  dimeCount = 0;
+int  nickleCount = 0;
+int  pennyCount = 0;
+
+//for counting coins value
+int coinValue = 0;
+double costToUse = 10.9;
+double payOutValue = 0;
+double processFeeTotal = 0;
+
+//for value of each coin
+int quarterValue = 25;
+int  dimeValue = 10;
+int nickleValue = 5;
+int pennyValue = 1;
+
+
+//capture user input
+int userCoinInput = 0;
+int coinValueToDimes = 0;
+int coinValueToNickle = 0;
+int coinValueToPennies = 0;
+
+//Get input and convert into the function
+cout << "Please enter the total value of the coins without a decimal place. " << endl;
+cin  >> userCoinInput;
+
+coinValue = userCoinInput;
+cout << "You entered " << userCoinInput << ".";
+coinValue = userCoinInput;
+
+//Quarters
+coinValueToDimes = coinValue % quarterValue;
+quarterCount = (coinValue/quarterValue);
+
+//Dimes
+coinValueToNickle = coinValueToDimes % dimeValue;
+dimeCount = (coinValueToDimes/dimeValue);
+
+//Nickles
+coinValueToPennies = coinValueToNickle % nickleValue;
+
+nickleCount = (coinValueToNickle/nickleValue);
+
+// Pennies
+pennyCount = (coinValueToPennies/pennyValue);
+
+
+// fee processing
+processFeeTotal= (userCoinInput*costToUse);
+payOutValue = (userCoinInput - processFeeTotal);
+
+// Outputting the values in statements
+cout << "You gave me " << quarterCount << " Quarters, and ";
+cout << dimeCount << " Dimes, and ";
+cout << nickleCount << " Nickles, and " ;
+cout << pennyCount << " Pennies. The original dollar amount was $ " << userCoinInput << endl;
+cout << "After a usage fee of " << costToUse << "% you now recieve $" << payOutValue << endl;
+cout << ". Now I’m a baller!!!";
+
+};
