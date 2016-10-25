8b4154e16544cf224069060451f6fdeefc792bd2
Jared Didier <jdidier001@student.butte.edu>
2016-10-25 02:27:18 +0000

    Lab 15 Submission

---
+++ lab15/Lab15.cpp
@@ -0,0 +1,85 @@
+/* 
+_________________________________________________
+Jared Didier
+email:jdidier001@student.butte.edu
+
+-------------------------------------------------
+
+_________________________________________________
+Lab 15
+10-20-16
+
+------------------------------------------------
+
+________________________________________________
+Fall 2016 CSCI 20 – Programming and Algorithms I
+instructor: A Browne
+email: browneap@butte.edu
+--------------------------------------------------
+
+________________________________________________
+Version # 1.0 - 10-20-16
+
+_________________________________________________
+**Notes**
+*************************************
+Submissions=
+    
+    git add lab1
+    git commit –a –m ‘description of your choice’
+    git push
+
+
+*/
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <chrono>
+
+using namespace std;
+
+//Initiize the Variables
+
+int art() {
+    
+    cout << "      Tying Digital Flies      " << endl;  
+    cout << " ______________________________" << endl;
+    cout << "   #####                       " << endl;
+    cout << "    #*#                        " << endl;
+    cout << "     *      $$                 " << endl;         
+    cout << "   #####$$$$$$$$$$$------      " << endl;
+    cout << "     *      $$   &             " << endl;  
+    cout << "    #*#           ]            " << endl;
+    cout << "   #####         &             " << endl;
+    cout << "              -^-              " << endl;
+    
+    cout << "Computerized FLies!" << endl << endl;
+    cout << "Copyright Jared W Didier 2016";
+};
+
+/*
+Create a menu program.  Your menu must have 10 different items for the user to choose from. The program should allow the user to choose an item until they say they are done.
+Requirements
+
+Use an array to hold how many of each item a user requests.
+Input
+
+As needed.
+Output
+
+As needed.
+Submission:
+
+Post your lab to GitHub.
+
+Submit to the Canvas assignment.  Put in any comments you want me to know.*/
+
+int main(){
+    
+    art();
+    
+    
+    
+}
+
